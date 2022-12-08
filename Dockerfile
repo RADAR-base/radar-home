b1b734fc7bd49c6a3ffee75a1e2b6414aac00830
Joris Borgdorff <joris@thehyve.nl>
2022-12-08 12:07:37 +0100

    Make built index.html configurable

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
 FROM --platform=$BUILDPLATFORM node:18 as builder
 
 WORKDIR /code
-COPY package*.json /code
+COPY package*.json /code/
 RUN npm install
 
 COPY src /code/src
+COPY tailwind.config.js /code/
 RUN npm run build
 
 FROM nginxinc/nginx-unprivileged:1.22-alpine
 
+COPY docker/30-env-subst.sh /docker-entrypoint.d/
 COPY --from=builder /code/dist /usr/share/nginx/html/
+COPY --from=builder --chown=101 /code/dist/index.html /usr/share/nginx/html/
+
+ENV S3_ENABLE="" S3_URL="" \
+  DASHBOARD_ENABLE="" DASHBOARD_URL="" \
+  UPLOAD_PORTAL_ENABLE="" \
+  REST_AUTHORIZER_ENABLE=""
---
+++ docker/30-env-subst.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+configure_block() {
+  local file="$1" prefix="$2" subst="$3" tmpfile="$(mktemp)"
+  local enable=$(printenv "${prefix}_ENABLE")
+  local subst_key="${prefix}_${subst}"
+  local subst_value=""
+  if [ -n "$enable" ] && [ -n "$subst" ]; then
+    subst_value=$(printenv "$subst_key")
+    if [ -z "$subst_value" ]; then
+      enable=""
+    fi
+  fi
+
+  if [ -z "$enable" ]; then
+    sed -r "/${prefix}_BEGIN/,/${prefix}_END/d" $file > $tmpfile
+  elif [ -n "$subst_value" ]; then
+    sed -r "s|${subst_key}|${subst_value}|" $file | sed -r "/${prefix}_(BEGIN|END)/d" > $tmpfile
+  else
+    sed -r "/${prefix}_(BEGIN|END)/d" $file > $tmpfile
+  fi
+  cat "$tmpfile" > "$file"
+  rm "$tmpfile"
+}
+
+cd /usr/share/nginx/html
+
+configure_block index.html "S3" "URL"
+configure_block index.html "DASHBOARD" "URL"
+configure_block index.html "UPLOAD_PORTAL"
+configure_block index.html "REST_AUTHORIZER"
